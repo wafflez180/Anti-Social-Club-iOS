8a951b295ff5f6439acf421d045692a4d7100830
Declan Hopkins <hopkins.declan@gmail.com>
2016-10-27 09:12:47 -0400

    Fix memory issue?

--- Anti-Social Club/PostTableViewCell.swift
+++ Anti-Social Club/PostTableViewCell.swift
@@ -144,22 +144,15 @@ class PostTableViewCell: UITableViewCell {
             
             if let image : UIImage = response.result.value
             {
-                print("image downloaded: \(image)")
-                self.post?.downloadedImage = image
                 self.postImageView.alpha = 0.0
                 self.postImageView.image = self.cropTo16by9Center(image: image)
                 UIView.animate(withDuration: 0.2, animations: {
                     self.postImageView.alpha = 1.0
                 })
-                /*
-                var newFrame = cell.productImageView.frame as CGRect
-                newFrame.size.width = cell.frame.size.width * 0.15
-                cell.productImageView.frame = newFrame
-                cell.layoutSubviews()
-                */
+
                 self.layoutSubviews()
-                self.layoutIfNeeded()
                 self.setNeedsLayout()
+                self.layoutIfNeeded()
             }
         }
     }
